60d9ac78d72db173bb85f3cadf0f95ae9e4cad3c
Ali Yarkin CANER <yarkincaner@gmail.com>
2021-05-15 11:57:37 +0300

    Refactoring

---
+++ commands/pause.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'pause',
+    description: "Pauses the current song",
+    execute(message, serverQueue) {
+        if (!message.member.voice.channel) {
+            return message.channel.send(
+                "You have to be in a voice channel to stop the music!"
+            );
+        }
+        if (!serverQueue) {
+            return message.channel.send("The queue is already empty!");
+        }
+
+        serverQueue.connection.dispatcher.pause(true);
+        serverQueue.songs[0].isPaused = true;
+        message.channel.send(`${serverQueue.songs[0].title} is paused`);
+    }
+}
